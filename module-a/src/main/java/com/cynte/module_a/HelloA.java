e10d7c480c89fb155dfdc2f6fc045616c8445d96
Mahmutihsandal <mahmutihsandal@gmail.com>
2025-01-29 23:35:32 +0300

    init commit

---
+++ module-a/src/main/java/com/cynte/module_a/HelloA.java
@@ -0,0 +1,7 @@
+package com.cynte.module_a;
+
+public class HelloA {
+    public static void sayHelloA() {
+        System.out.println("HelloA, World!");
+    }
+}
---
+++ module-b/src/main/java/com/cynte/module_b/HelloB.java
@@ -0,0 +1,7 @@
+package com.cynte.module_b;
+
+public class HelloB {
+    public static void sayHelloB() {
+        System.out.println("HelloB, World!");
+    }
+}
